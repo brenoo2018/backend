8b1bb1b98fd86c9a103367777bb1ca4fbc215bc0
brenoo2018 <thaynanbreno@gmail.com>
2020-09-23 10:07:18 -0300

    create users

---
+++ src/controller/UsersController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+import { uuid } from 'uuidv4';
+import { hash } from 'bcryptjs';
+import knex from '../database/connection';
+
+export async function create(request: Request, response: Response) {
+  try {
+    const { username, email, password } = request.body;
+
+    const checkUsersExists = await knex('users').where({ email });
+
+    if (checkUsersExists) {
+      return response.json({ message: 'User already exists' });
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await knex('users').insert({
+      uuid: uuid(),
+      username,
+      email,
+      password: passwordHash,
+    });
+
+    return response.json({ message: 'Success' });
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,12 +2,8 @@ import { Router } from 'express';
 
 const usersRouter = Router();
 
-usersRouter.get('/list', async (request, response) => {
-  try {
-    return response.send('foi users');
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
-});
+import * as UsersController from '../controller/UsersController';
+
+usersRouter.post('/', UsersController.create);
 
 export default usersRouter;
